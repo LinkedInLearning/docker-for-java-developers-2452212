57deb2e63c19e5e07d33fc04a89baf057b5e0e36
miellyellen <maryellen.bowman@gmail.com>
2022-04-03 13:12:17 -0400

    removed volumedemo added portdemo

---
+++ portdemo/src/main/java/com/example/portdemo/PortdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.portdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortdemoApplication.class, args);
+	}
+
+}
